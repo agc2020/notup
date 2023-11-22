f13f24905dd1433df839680c318043fcc0253c54
agc2020 <gabardo@outlook.com>
2023-11-22 17:07:45 -0300

    Update2 Teste

---
+++ php/autenticacao2.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'vendor/autoload.php';
+use Firebase\JWT\JWT;
+
+$secret_key = "SUA_CHAVE_SECRETA";
+
+$jwt = getallheaders()['Authorization'] ?? '';
+
+if (!$jwt) {
+    http_response_code(401);
+    echo "Acesso negado. Token não fornecido.";
+    exit;
+}
+
+try {
+    $decoded = JWT::decode($jwt, $secret_key, array('HS256'));
+    // Token válido, o usuário está autenticado
+} catch (Exception $e) {
+    http_response_code(401);
+    echo "Acesso negado: " . $e->getMessage();
+    exit;
+}
+?>
---
+++ php/login.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'db_connect.php';
+require_once 'vendor/autoload.php';
+use Firebase\JWT\JWT;
+
+$email = $_POST['email'] ?? '';
+$senha = $_POST['senha'] ?? '';
+
+$sql = "SELECT id, senha_criptografada FROM Usuarios WHERE email = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    if (password_verify($senha, $row['senha_criptografada'])) {
+        $payload = [
+            "user_id" => $row['id'],
+            "email" => $email,
+            "exp" => time() + 3600
+        ];
+
+        $jwt = JWT::encode($payload, $secret_key);
+        echo $jwt;
+    } else {
+        echo "Senha incorreta";
+    }
+} else {
+    echo "Usuário não encontrado";
+}
+?>
