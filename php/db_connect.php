beb374e578dab432bd30b7330bb5edef74d14536
agc2020 <gabardo@outlook.com>
2023-11-23 03:07:51 -0300

    23.11.2023 - Update 2

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "sua_senha";
+$password = "";
 $dbname = "base1";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
