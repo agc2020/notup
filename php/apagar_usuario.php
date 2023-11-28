fac153d4dbb683d1533fc93d63618312cd913779
agc2020 <gabardo@outlook.com>
2023-11-27 21:31:16 -0300

    27.11.2023 - Apagar usuário php

---
+++ php/apagar_usuario.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'db_connect.php';
+
+if (!isset($_SESSION['email'])) {
+    header("Location: ../pages/auth.html");
+    exit();
+}
+
+$email = $_SESSION['email'];
+
+$sql = "DELETE FROM Usuarios WHERE email = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $email);
+
+if ($stmt->execute()) {
+    session_destroy();
+    header("Location: ../pages/auth.html?success=accountdeleted");
+    exit();
+} else {
+    header("Location: ../pages/dashboard.html?error=sqlerror");
+    exit();
+}
+?>
